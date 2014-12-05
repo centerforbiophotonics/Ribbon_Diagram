a4992ef99aaac8a3adbd84998c95f73ca10d204e
matt-steinwachs <matt.steinwachs@gmail.com>
2014-12-04 16:26:05 -0800

    Commented out unfinished paperclip config in Diagram model that was causing exception.

--- app/models/diagram.rb
+++ app/models/diagram.rb
@@ -1,6 +1,6 @@
 class Diagram < ActiveRecord::Base
-  has_attached_file :data_file,
-                    :storage => :s3,
-                    :s3_permissions => :private,
-                    :path => "data_files/#{ Institution.find(institution.id).name }/:filename"
+  #has_attached_file :data_file,
+  #                  :storage => :s3,
+  #                  :s3_permissions => :private,
+  #                  :path => "data_files/#{ Institution.find(institution_id).name }/:filename"
 end
