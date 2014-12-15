1ff38493a8171632704b57c18eec856d5e93ffa4
matt-steinwachs <matt.steinwachs@gmail.com>
2014-12-15 15:43:00 -0800

    Added deploy folder and before_restart.rb to use Chef deployment hooks to precompile assets on AWS deploy.

---
+++ deploy/before_restart.rb
@@ -0,0 +1,8 @@
+Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env}...")
+Chef::Log.info("********************************")
+
+execute "rake assets:precompile" do
+  cwd release_path
+  command "bundle exec rake assets:precompile"
+  environment "RAILS_ENV" => rails_env
+end
