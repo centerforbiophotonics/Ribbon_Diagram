2b244bb75f09f4dd74c755a917da5c4a5f520363
Matthew Steinwachs <matt.steinwachs@gmail.com>
2015-01-06 12:46:07 -0800

    Fixed loop which determined the min and max for numeric demographics.

--- bennett_converter.rb
+++ bennett_converter.rb
@@ -9,3 +9,14 @@ data_hash.each do |key, val|
 end
 
 File.open("/Users/steinwam/Desktop/TEA Data for testing/BUSTEMribbon.JSON", 'w') { |file| file.write(new_object.to_json) }
+
+
+file = File.read('/Users/steinwam/Desktop/TEA Data for testing/USASK_ribbon_20150103.json')
+
+data_hash = JSON.parse(file)
+
+puts data_hash["STUDENTS"].size
+
+File.open("/Users/steinwam/Desktop/TEA Data for testing/USASK_ribbon_20150103.json", 'w') { |file| file.write(data_hash.to_json) }
+
+
