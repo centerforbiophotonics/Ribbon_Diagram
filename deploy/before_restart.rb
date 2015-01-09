89767c6c458a10b02b1e07657ddd50b831864b03
matt-steinwachs <matt.steinwachs@gmail.com>
2015-01-09 13:18:32 -0800

    another tweak to before_restart.rb

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,10 +1,11 @@
-Chef::Log.info("Precompiling assets.")
-Chef::Log.info("********************************")
-
 rails_env = new_resource.environment["RAILS_ENV"]
+
+Chef::Log.info("Precompiling assets.")
+Chef::Log.info(rails_env)
+Chef::Log.info("********************************")
 
 execute "rake assets:precompile" do
   cwd release_path
   command "RAILS_ENV=production bundle exec rake assets:precompile"
-  environment "RAILS_ENV" => rails_env
+  environment "RAILS_ENV" => "production"
 end
