2bc552d3983dce863b2a36ba3f981385490cbd34
matt-steinwachs <matt.steinwachs@gmail.com>
2015-01-09 13:04:58 -0800

    Added manual (possibly redundant) specification of rails environment before the asset precompile task in the before_restart.rb deploy action.

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -5,6 +5,6 @@ rails_env = new_resource.environment["RAILS_ENV"]
 
 execute "rake assets:precompile" do
   cwd release_path
-  command "bundle exec rake assets:precompile"
+  command "RAILS_ENV=production bundle exec rake assets:precompile"
   environment "RAILS_ENV" => rails_env
 end
