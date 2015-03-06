5d5cdc5b199f228daab31193872a0c72e289a83d
matt-steinwachs <matt.steinwachs@gmail.com>
2015-03-06 12:17:15 -0800

    Fixed before_symlink.rb.  I had forgetten to change it after getting it to work in GORP (another project).  Removed previous logging.

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,11 +1,5 @@
 rails_env = new_resource.environment["RAILS_ENV"]
-devise = new_resource.environment["DEVISE_SECRET_KEY"]
 
 Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env} in #{release_path}.")
-Chef::Log.info("Devise Key=#{devise}.")
 
-execute "rake assets:precompile" do
-  cwd release_path
-  command "bundle exec rake assets:precompile"
-  environment "RAILS_ENV" => rails_env
-end
+run "cd #{release_path} && RAILS_ENV=#{rails_env} bundle exec rake assets:precompile"
