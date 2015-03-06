f0a8aba4f555aad7b69628f82895bd0fff39d18b
matt-steinwachs <matt.steinwachs@gmail.com>
2015-03-06 11:46:34 -0800

    Added logging of devise key to before_symlink deploy to test it's presense during asset precompiling.

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,6 +1,8 @@
 rails_env = new_resource.environment["RAILS_ENV"]
+devise = new_resource.environment["DEVISE_SECRET_KEY"]
 
 Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env} in #{release_path}.")
+Chef::Log.info("Devise Key=#{devise}.")
 
 execute "rake assets:precompile" do
   cwd release_path
