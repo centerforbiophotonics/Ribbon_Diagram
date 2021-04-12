99c7475ecff388d9f740d088b527af1e5db0f3e3
Matt Steinwachs <Matt.Steinwachs@gmail.com>
2021-04-12 12:36:58 -0700

    Update diagram.rb

--- app/models/diagram.rb
+++ app/models/diagram.rb
@@ -5,11 +5,11 @@ class Diagram < ActiveRecord::Base
   has_many :user_diagrams, :dependent => :delete_all
   has_many :users, :through => :user_diagrams
 
-  has_many :data_files, :dependent => :destroy
+  # has_many :data_files, :dependent => :destroy
 
   belongs_to :creator, :class_name => "User"
 
-  accepts_nested_attributes_for :data_files, :reject_if => proc { |attributes| attributes['data_file'].blank? || attributes['name'].blank? }
+  # accepts_nested_attributes_for :data_files, :reject_if => proc { |attributes| attributes['data_file'].blank? || attributes['name'].blank? }
 
   validates_presence_of :name, :category
 
