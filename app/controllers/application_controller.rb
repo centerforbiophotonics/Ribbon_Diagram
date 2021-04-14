2e9f7bea16fc6d82dbaab59a03736c6a9ff773b5
Matt Steinwachs <Matt.Steinwachs@gmail.com>
2021-04-14 16:50:36 -0700

    Fixed signup strong params, fixed two csv format (order dependent)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   include Pundit
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery :with => :exception
@@ -40,6 +44,10 @@ class ApplicationController < ActionController::Base
   #   devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me, :institution_id, :name, :title, :department) }
   #   devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password, :institution_id, :name, :title, :department) }
   # end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:title, :department, :name, :institution_id])
+  end
 
   private
 
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,5 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-# before_action :configure_sign_up_params, only: [:create]
+  before_action :sign_up_params, only: [:create]
 # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -39,9 +39,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # You can put the params you want to permit in the empty array.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.for(:sign_up) << :attribute
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:title, :department])
+  end
+
+  def sign_up_params
+   params.require(:user).permit(:title, :department, :email, :password, :password_confirmation)
+  end
 
   # You can put the params you want to permit in the empty array.
   # def configure_account_update_params
